94bd813f108ec9b6e2c5a16969a5660b2375559a
Hari Mohan <hari1111mohan@gmail.com>
2024-05-06 08:01:05 +0530

    Added user routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
-
-
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUP from "./Pages/SignUp";
 
 function App() {
- 
-
   return (
     <>
-      <h1 className='text-red-600'>Helo world</h1>
-      
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUP />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
