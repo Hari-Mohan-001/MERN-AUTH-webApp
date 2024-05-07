c6dcffae53206d43ade2ed524e132613378b029e
Hari Mohan <hari1111mohan@gmail.com>
2024-05-07 19:03:05 +0530

    made profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 
 import SignInPage from "./Pages/SignInPage";
-import SignUpPage from './Pages/SignUpPage'
+import SignUpPage from "./Pages/SignUpPage";
 import ProfilePage from "./Pages/ProfilePage";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
-    
     <>
-      <Routes> 
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/signIn" element={<SignInPage />} />
         <Route path="/signUp" element={<SignUpPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Routes>
     </>
   );
--- client/src/Components/User/Header.jsx
+++ client/src/Components/User/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 
 const Header = () => {
+  const {currentUser} = useSelector((state)=> state.user)
   return (
     <div className='bg-slate-300'>
         <div className='flex justify-between items-center max max-w-7xl mx-auto p-3'>
             <h1 className='font-bold'>User Management</h1>
             <ul className='flex gap-4'>
                 <Link to={'/'}><li>Home</li></Link>
-                <Link to={'/signIn'}><li>SignIn</li></Link>
-                <Link to={'/profile'}><li>Profile</li></Link>  
+                <Link to={'/profile'}>
+                  {currentUser ? (
+                    <img className='w-7 h-7 rounded-full object-cover' src={currentUser.profileImage} alt="image" />
+                  ):
+                  (<li>SignIn</li>
+                     )} 
+                  </Link>
+                  
                 
             </ul>
         </div>
